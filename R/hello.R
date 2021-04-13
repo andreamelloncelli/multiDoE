1515765b338abbb4e0f84821899f6f56d30c0134
Andrea Melloncelli <andrea.melloncelli@gmail.com>
2021-04-13 13:20:20 +0200

    add roxygen configuration

--- R/hello.R
+++ R/hello.R
@@ -1,6 +1,6 @@
 # Hello, world!
 #
-# This is an example function named 'hello' 
+# This is an example function named 'hello'
 # which prints 'Hello, world!'.
 #
 # You can learn more about package authoring with RStudio at:
@@ -12,7 +12,12 @@
 #   Install Package:           'Ctrl + Shift + B'
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
+#   Snippet Roxygen:           'Ctrl + Shift + Alt + R'
 
+#' Title
+#'
+#' @return NULL
+#' @export
 hello <- function() {
   print("Hello, world!")
 }
