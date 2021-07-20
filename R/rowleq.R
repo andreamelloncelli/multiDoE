ef8456e74f2593e2288d9d86fd1ba03c5f1b51de
Francesca Cucchi <fra.cucchi@icloud.com>
2021-07-20 10:22:20 +0200

    rowleq implementato anche nella versione originale

--- R/rowleq.R
+++ R/rowleq.R
@@ -8,8 +8,21 @@
 #
 # res = (A(i) <= B(i));
 
-
 rowleq <- function(A, B) {
+  i = 1
+  while (A[i] == B[i] & i < length(A)) {
+    i = i + 1
+  }
+  res <- A[i] <= B[i]
+  return(res)
+}
+
+
+# verificare se può andare bene questa
+rowleq2 <- function(A, B) {
   argmin = which.min(A==B)   # restituisce il primo FALSE (0)
   return(A[argmin] <= B[argmin])
 }
+
+a <- c(1,2,2,5)
+b <- c(1,2,2,4)
