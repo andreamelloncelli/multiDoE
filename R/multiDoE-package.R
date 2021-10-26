735fdeb96c285d181c9731f8da80089555d84de7
Andrea Melloncelli <andrea.melloncelli@gmail.com>
2021-10-26 17:42:44 +0200

    add a draft documentation of the package itself

---
+++ R/multiDoE-package.R
@@ -0,0 +1,8 @@
+# Roxygen docs:
+# https://roxygen2.r-lib.org/articles/rd.html#packages
+
+#' @details
+#' XXX TODO FIX ME XXXX The only function you're likely to need from roxygen2 is [roxygenize()].
+#' Otherwise refer to the vignettes to see how to format the documentation.
+#' This comes from the R/multiDOE-package.R
+"_PACKAGE"
