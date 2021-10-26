5eb90c969ec3526c272fe357fbcd9fcfb745eb05
Andrea Melloncelli <andrea.melloncelli@gmail.com>
2021-10-26 16:49:22 +0200

    add documentatio-build.R dev procedures

---
+++ dev/documentation-build.R
@@ -0,0 +1,25 @@
+
+# create package PDF document  --------------------------------------------
+
+# update .Rd files (notice the Warning messages!!!)
+devtools::document(roclets = c('rd', 'collate', 'namespace'))
+# create the manual
+devtools::build_manual()
+# open the output file (or do it through the file browser)
+system(paste0("xdg-open ../multiDoE_0.1.0.pdf"))
+# in case it does not work you can debug it with:
+devtools::check(manual=TRUE)
+
+
+# get PDF help ------------------------------------------------------------
+
+# update the docs
+devtools::document(roclets = c('rd', 'collate', 'namespace'))
+# install also the docs
+devtools::install()
+# select a function
+fun <- "MSOpt"
+# create a PDF document for that function only
+help(fun, package = "multiDoE", help_type = "pdf")
+# open the output file (or do it through the file browser)
+system(paste0("xdg-open ",  fun, ".pdf"))
