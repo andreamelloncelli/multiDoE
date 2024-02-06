91b2673e87bb5bacfe8c096d4e7d6ab03a0156b3
Andrea Melloncelli <andrea.melloncelli@gmail.com>
2024-02-06 12:07:34 +0100

    cran checks work the final package file not the folder.

--- dev/run-cran-check.sh
+++ dev/run-cran-check.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-R CMD check --as-cran .
+PACKAGE_FILE=multiDoE_0.9.0.tar.gz
+
+if [[ ! -f $PACKAGE_FILE ]]; then
+  echo "File not found: $PACKAGE_FILE. Run 'R CMD build .' first. Then check the file name."
+  exit 1
+fi
+
+# CRAN suggests to check the tar.gz and not the folder
+R CMD check --as-cran $PACKAGE_FILE
